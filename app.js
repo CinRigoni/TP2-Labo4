6585b818f6c99ba40485036f7db39cb2cdd2873f
CinRigoni <81648801+CinRigoni@users.noreply.github.com>
2022-04-08 19:43:56 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const { default: axios } = require("axios");
+const axios = require("axios");
 const mysql = require('mysql')
 
 //Conexion a DB
@@ -14,31 +14,53 @@ connection.connect((error) => {
     console.log("Conexion correcta!")
 })
 
-function llamaAxios(){
-    axios.get(`https://restcountries.com/v2/callingcode/1`, {
+async function llamada(){
+    let json = null
+    await axios.get(`https://restcountries.com/v2/callingcode/1`, {
         responseType: 'json'
     })
     .then(res => {
-        //console.log(res)
-        let json = res.data
-        console.log(json[0].name)
-        /* for(i=0;i<=1;i++){
+        json = res.data
+        //console.log(json)
+        /* for(i=0;i<=5;i++){
             console.log(json[i].name)
-            let nombrePais = json[i].name
-            let capPais = json[i].capital
-            let region = json[i].region
-            let poblac = json[i].population
-            let lat = json[i].latlng[0]
-            let long = json[i].latlng[1]
-            let codPais = json[i].numericCode
-            console.log(`${nombrePais} - ${capPais} - ${region} - ${poblac} - ${lat} - ${long} - ${codPais}`)
         } */
     })
     .catch(function(err) {
         console.log("No pudo conectarse");
     })
+    return json
 }
 
-llamaAxios()
+const datos = llamada()
+console.log(datos)
+
+/* for(i=0;i<=300;i++){
+    let nombrePais = json[i].name
+    let capPais = json[i].capital
+    let region = json[i].region
+    let poblac = json[i].population
+    let lat = json[i].latlng[0]
+    let long = json[i].latlng[1]
+    let codPais = json[i].numericCode
+    let pais = null
+    //Busco si está ese país
+    connection.query(`SELECT * FROM pais WHERE codigoPais = ${codPais}`, (err, row) => {
+        pais = row
+    })
+    if(pais){
+        //Actualizo pais encontrado
+        connection.query(`UPDATE ${pais} SET nombrePais = ${nombrePais}, capitalPais = ${capPais}, region = ${poblac}, latitud = ${lat}, longitud = ${long} WHERE codigoPais = ${codPais}`, (err, row) =>{
+            if(err) throw err
+            console.log("País actualizado")
+        })
+    }else{
+        //Agrego pais
+        connection.query(`INSERT INTO pais (nombrePais, capitalPais, region, poblacion, latitud, longitud, codigoPais) VALUES (${nombrePais}, ${capPais}, ${region}, ${poblac}, ${lat}, ${long}, ${codPais})`, (err, row) =>{
+            if(err) throw err
+            console.log("País insertado")
+        })
+    }
+} */
 
 connection.end()
